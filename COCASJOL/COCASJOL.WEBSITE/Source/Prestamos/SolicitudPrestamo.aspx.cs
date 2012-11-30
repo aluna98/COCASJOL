f426fabbd2cd267745dc8a9605bb9c6ce40acab5
xapiz <andrea_1578@unitec.edu>
2012-11-30 16:48:03 -0600

    Agregando Logica de Solicitudes de Prestamos

--- COCASJOL/COCASJOL.WEBSITE/Source/Prestamos/SolicitudPrestamo.aspx.cs
+++ COCASJOL/COCASJOL.WEBSITE/Source/Prestamos/SolicitudPrestamo.aspx.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using COCASJOL.LOGIC;
+using System.Data;
+using System.Data.Objects;
+using Ext.Net;
+
+using COCASJOL.LOGIC.Web;
+using COCASJOL.LOGIC.Seguridad;
+using COCASJOL.LOGIC.Prestamos;
 
 namespace COCASJOL.Website.Source.Prestamos
 {
@@ -12,6 +20,14 @@ namespace COCASJOL.Website.Source.Prestamos
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void SolicitudesSt_Reload(object sender, StoreRefreshDataEventArgs e)
+        {
+            SolicitudesLogic prestamo = new SolicitudesLogic();
+            var store1 = this.SolicitudesGriP.GetStore();
+            store1.DataSource = prestamo.getData();
+            store1.DataBind();
         }
     }
 }
